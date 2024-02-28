90d4b4bc801d7079905a6f926d3bba81be3b005a
akasha-rec <uspira@naver.com>
2024-02-28 11:56:44 +0900

    수정과 추가

--- BOJ/fundamental.py
+++ BOJ/fundamental.py
@@ -8,6 +8,6 @@
 # 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
 A, B = input().split()
 A, B = int(A), int(B)
-# / : 결과 float. // : 결과 int
+# / : 나눗셈 결과 float. // : 결과 int
 print(A+B, A-B, A*B, A//B, A%B, sep="\n")
 
--- prof_ty.py/review.py
+++ prof_ty.py/review.py
@@ -5,3 +5,24 @@ print(type(b))
 
 s = "abcde"
 print(s[:2], s[2:])
+# len 리스트 길이
+print(len(s))
+print(type(s[:2])) 
+
+# <class 'str'>
+for s in "abcdef":
+    print(s)
+
+# range
+for i in range(1, 6):
+    print(i)
+
+# class 'tuple' 
+for a in (1, 2, 3, 4, 5):
+    print(a)
+
+for i in range(1, 6):
+    if (i % 2 == 0):
+        print(i, "even")
+    else:
+        print(i, "odd")
