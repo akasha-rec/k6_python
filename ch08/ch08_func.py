3a87c50c84a40ab5ec0e619e2a894a5c2916ec03
akasha-rec <uspira@naver.com>
2024-03-04 09:45:06 +0900

    함수 추가 설명

--- ch08/ch08_func.py
+++ ch08/ch08_func.py
@@ -26,5 +26,26 @@ def apply_operation(operation, x, y):
 
 result = apply_operation(add, 3, 4)
 result2 = apply_operation(minus, 4, 3)
+print(f"add 결과 : {result}, minus 결과 : {result2}")
+
+#map(), filter() 함수 : 리스트 or tuple
+#> map(함수, 데이터) : 머신러닝, 딥러닝 때 사용
+#>> filter(함수, 데이터)
+
+# def power(item):
+#     return item * item
+
+# def under_three(item):
+#     return item < 3
 
 #lambda
+power = lambda x: x*x
+under_three = lambda x: x < 3
+
+lst = [1, 2, 3, 4, 5]
+map_list = map(power, lst)
+print(f"map() 함수 적용결과 : {list(map_list)}")
+
+filter_list = filter(under_three, lst)
+print(f"filter() 함수 적용결과 : {list(filter_list)}")
+
